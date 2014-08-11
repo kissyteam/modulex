4c11923dcf2d6ba3015e81a745d910e25f92e3a3
yiminghe <yiminghe@gmail.com>
2014-08-11 20:19:09 +0800

    update nodejs

--- build/modulex-nodejs.js
+++ build/modulex-nodejs.js
@@ -1256,8 +1256,8 @@ var modulex = (function (undefined) {
         if (css) {
             node.href = url;
             node.rel = 'stylesheet';
-            // can not set, else test fail
-            // node.media = 'async';
+            // set media to something non-matching to ensure it'll fetch without blocking render
+            node.media = 'async';
         } else {
             node.src = url;
             node.async = true;
@@ -1267,6 +1267,11 @@ var modulex = (function (undefined) {
             var index = error;
             var fn;
             clearTimer();
+            // set media back to `all` so that the stylesheet applies once it loads
+            // https://github.com/filamentgroup/loadCSS
+            if (css) {
+                node.media = 'all';
+            }
             Utils.each(jsCssCallbacks[url], function (callback) {
                 if ((fn = callback[index])) {
                     fn.call(node);
@@ -2302,5 +2307,87 @@ var modulex = (function (undefined) {
     }
     mx.add('logger-manager', function () {
         return mx.LoggerMangaer;
+    });
+})(modulex);
+/**
+ * @ignore
+ * implement getScript for nodejs synchronously.
+ * so loader need not to be changed.
+ * @author yiminghe@gmail.com
+ */
+(function (mx) {
+    /*global require*/
+    var fs = require('fs');
+    var Utils = mx.Loader.Utils;
+    var vm = require('vm');
+    mx.getScript = function (url, success, charset) {
+        var error;
+        if (typeof success === 'object') {
+            charset = success.charset;
+            error = success.error;
+            success = success.success;
+        }
+        if (Utils.endsWith(url, '.css')) {
+            mx.log('node js can not load css: ' + url, 'warn');
+            if (success) {
+                success();
+            }
+            return;
+        }
+        if (!fs.existsSync(url)) {
+            var e = 'can not find file ' + url;
+            mx.log(e, 'error');
+            if (error) {
+                error(e);
+            }
+            return;
+        }
+        try {
+            // async is controlled by async option in use
+            // sync load in getScript, same as cached load in browser environment
+            var mod = fs.readFileSync(url, charset);
+            // code in runInThisContext unlike eval can not access local scope
+            // noinspection JSUnresolvedFunction
+            // use path, or else use url will error in nodejs debug mode
+            var factory = vm.runInThisContext('(function(modulex, requireNode){' + mod + '})', url);
+            factory(mx, function (moduleName) {
+                return require(Utils.normalizePath(url, moduleName));
+            });
+            if (success) {
+                success();
+            }
+        } catch (e) {
+            mx.log('in file: ' + url, 'error');
+            mx.log(e.stack, 'error');
+            if (error) {
+                error(e);
+            }
+        }
+    };
+    mx.modulex = mx;
+    /*global module*/
+    if (typeof module !== 'undefined') {
+        module.exports = mx;
+    }
+    mx.config({
+        charset: 'utf-8',
+        /*global __dirname*/
+        base: __dirname.replace(/\\/g, '/').replace(/\/$/, '') + '/'
+    });
+    // require synchronously in node js
+    mx.nodeRequire = function (modName) {
+        var ret = [];
+        mx.use([modName], {
+            success: function () {
+                ret = [].slice.call(arguments, 1);
+            },
+            sync: true
+        });
+        return ret[0];
+    };
+    mx.config('packages', {
+        core: {
+            filter: ''
+        }
     });
 })(modulex);
--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,20 @@ gulp.task('default', function () {
     files.forEach(function (f, i) {
         files[i] = './lib/' + f;
     });
+
     gulp.src(files)
         .pipe(concat('modulex.js'))
+        .pipe(gulp.dest('./build'));
+
+    gulp.src(['./build/modulex.js','./lib/nodejs.js'])
+        .pipe(concat('modulex-nodejs.js'))
         .pipe(gulp.dest('./build'));
 });
 
 var express = require('express');
 var comboHandler = require('combo-handler');
 var path = require('path');
+
 gulp.task('server', function () {
     var app = express();
     app.use('/modulex/', comboHandler({
@@ -31,7 +37,7 @@ gulp.task('server', function () {
             next();
         }
     });
-    app.use('/modulex/', express['directory'](__dirname, {
+    app.use('/modulex/', express.directory(__dirname, {
         hidden: true
     }));
     app.use('/modulex/', express['static'](__dirname, {
